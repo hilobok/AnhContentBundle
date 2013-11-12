a26db4b6792adb7d18dac0c346133ef526d67519
Andrew Hilobok <hilobok@gmail.com>
2013-11-12 14:08:56 +0200

    Refactored DefaultController to use BaseController, refactored templates

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,48 +2,180 @@
 
 namespace Anh\ContentBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class DefaultController extends Controller
+class DefaultController extends BaseController
 {
-    public function paperListAction($section)
+    /**
+     * Lists all papers in section
+     *
+     * @param string $section
+     *
+     * @return Response
+     */
+    public function listPapersAction($section)
     {
-        $papers = $this->container->get('anh_content.manager.paper')
-            ->findPublishedInSection($section)
-        ;
-
-        return $this->render('AnhContentBundle:Default:list.html.twig', array(
+        return $this->render('AnhContentBundle:Default:listPapers.html.twig', array(
             'section' => $section,
-            'papers' => $papers
+            'papers' => $this->getPublishedPapers($section)
         ));
     }
 
-    public function paperViewAction($section, $slug)
+    /**
+     * Paginates papers in section
+     *
+     * @param string $section
+     * @param integer $page
+     * @param integer $limit
+     *
+     * @return Response
+     */
+    public function paginatePapersAction($section, $page = 1, $limit = 10)
     {
-        $paper = $this->container->get('anh_content.manager.paper')
-            ->findInSectionBySlug($section, $slug)
+        $pager = $this->paginatePublishedPapers($section, $page, $limit)
+            ->setUrl(str_replace('0', '{page}',
+                $this->contentUrl('papers', $section, array(
+                    'page' => 0
+                ))
+            ))
         ;
 
-        return $this->render('AnhContentBundle:Default:view.html.twig', array(
+        return $this->render('AnhContentBundle:Default:paginatePapers.html.twig', array(
             'section' => $section,
-            'paper' => $paper
+            'pager' => $pager
         ));
     }
 
-    public function categoryViewAction($section, $slug)
+    /**
+     * Lists all categories in section
+     *
+     * @param string $section
+     *
+     * @return Response
+     */
+    public function listCategoriesAction($section)
     {
-        $category = $this->container->get('anh_content.manager.category')
-            ->findInSectionBySlug($section, $slug)
+        $sections = $this->getSections();
+
+        if (!$sections[$section]['category']) {
+            throw new \InvalidArgumentException(
+                sprintf("Categories not enabled for section '%s'.", $section)
+            );
+        }
+
+        return $this->render('AnhContentBundle:Default:listCategories.html.twig', array(
+            'section' => $section,
+            'categories' => $this->getCategories($section)
+        ));
+    }
+
+    /**
+     * Paginates categories in section
+     *
+     * @param string $section
+     * @param integer $page
+     * @param integer $limit
+     *
+     * @return Response
+     */
+    public function paginateCategoriesAction($section, $page = 1, $limit = 10)
+    {
+        $sections = $this->getSections();
+
+        if (!$sections[$section]['category']) {
+            throw new \InvalidArgumentException(
+                sprintf("Categories not enabled for section '%s'.", $section)
+            );
+        }
+
+        $pager = $this->paginateCategories($section, $page, $limit)
+            ->setUrl(str_replace('0', '{page}',
+                $this->contentUrl('categories', $section, array(
+                    'page' => 0
+                ))
+            ))
         ;
 
-        $papers = $this->container->get('anh_content.manager.paper')
-            ->findPublishedInSectionAndCategory($section, $category)
-        ;
+        return $this->render('AnhContentBundle:Default:paginateCategories.html.twig', array(
+            'section' => $section,
+            'pager' => $pager
+        ));
+    }
 
-        return $this->render('AnhContentBundle:Default:category.html.twig', array(
+    /**
+     * View paper
+     *
+     * @param string $section
+     * @param string $slug
+     *
+     * @return Response
+     */
+    public function viewPaperAction($section, $slug)
+    {
+        return $this->render('AnhContentBundle:Default:viewPaper.html.twig', array(
+            'section' => $section,
+            'paper' => $this->getPaper($section, $slug)
+        ));
+    }
+
+    /**
+     * Lists papers in category
+     *
+     * @param string $section
+     * @param string $slug
+     *
+     * @return Response
+     */
+    public function viewCategoryAction($section, $slug)
+    {
+        $sections = $this->getSections();
+
+        if (!$sections[$section]['category']) {
+            throw new \InvalidArgumentException(
+                sprintf("Categories not enabled for section '%s'.", $section)
+            );
+        }
+
+        $category = $this->getCategory($section, $slug);
+        $papers = $this->getPublishedPapersInCategory($section, $category);
+
+        return $this->render('AnhContentBundle:Default:viewCategory.html.twig', array(
             'section' => $section,
             'category' => $category,
             'papers' => $papers
+        ));
+    }
+
+    /**
+     * Paginates papers in category
+     *
+     * @param string $section
+     * @param integer $page
+     * @param integer $limit
+     *
+     * @return Response
+     */
+    public function paginateCategoryAction($section, $slug, $page = 1, $limit = 10)
+    {
+        $sections = $this->getSections();
+
+        if (!$sections[$section]['category']) {
+            throw new \InvalidArgumentException(
+                sprintf("Categories not enabled for section '%s'.", $section)
+            );
+        }
+
+        $category = $this->getCategory($section, $slug);
+        $pager = $this->paginatePublishedPapersInCategory($section, $category, $page, $limit)
+            ->setUrl(str_replace('0', '{page}',
+                $this->contentUrl('category', $section, $category->getUrlParameters() + array(
+                    'page' => 0
+                ))
+            ))
+        ;
+
+        return $this->render('AnhContentBundle:Default:paginateCategory.html.twig', array(
+            'section' => $section,
+            'category' => $category,
+            'pager' => $pager
         ));
     }
 }
