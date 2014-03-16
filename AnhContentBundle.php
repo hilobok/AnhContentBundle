2ade5b420e6761cd5de1272fc9311a6e0197510d
Andrew Hilobok <hilobok@gmail.com>
2014-03-16 17:55:59 +0200

    Mark version 1.1.5

--- AnhContentBundle.php
+++ AnhContentBundle.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AnhContentBundle extends Bundle
 {
-    const VERSION = 'v1.1.4';
+    const VERSION = 'v1.1.5';
     const TITLE = 'AnhContentBundle';
     const DESCRIPTION = 'Bundle for content management';
 
