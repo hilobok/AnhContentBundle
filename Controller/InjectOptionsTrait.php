8585c27475e60a10bb5a3d43265195d05bbf0597
Andrew Hilobok <hilobok@gmail.com>
2014-10-14 16:43:34 +0300

    Fixed injecting options

--- Controller/InjectOptionsTrait.php
+++ Controller/InjectOptionsTrait.php
@@ -14,13 +14,22 @@ trait InjectOptionsTrait
         ));
     }
 
+    /**
+     * Recursive analog of $array1 + $array2
+     *
+     * @param  array  $array1
+     * @param  array  $array2
+     * @return array
+     */
     protected function merge(array $array1, array $array2)
     {
         $merged = $array1;
 
         foreach ($array2 as $key => $value) {
-            if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
-                $merged[$key] = array_merge($merged[$key], $value);
+            if (array_key_exists($key, $merged)) {
+                if (is_array($merged[$key]) && is_array($value)) {
+                    $merged[$key] = $this->merge($merged[$key], $value);
+                }
             } else {
                 $merged[$key] = $value;
             }
